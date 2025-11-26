6b982454e891b263831b60c30362e18b20faeb60
Fatimah Albariqi <fatemahh744@gmail.com>
2025-11-26 19:41:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,39 @@ const nextBtn = document.querySelector('.next');
 let index = 0;
 
 function showSlide() {
-    slides.style.transform = `translateX(-${index * 100}%)`;
+    if (slides) {
+        slides.style.transform = `translateX(-${index * 100}%)`;
+    }
 }
 
-nextBtn.addEventListener('click', () => {
-    index = (index + 1) % images.length;
-    showSlide();
-});
+if (nextBtn && prevBtn && slides) {
+    nextBtn.addEventListener('click', () => {
+        index = (index + 1) % images.length;
+        showSlide();
+    });
 
-prevBtn.addEventListener('click', () => {
-    index = (index - 1 + images.length) % images.length;
-    showSlide();
-});
+    prevBtn.addEventListener('click', () => {
+        index = (index - 1 + images.length) % images.length;
+        showSlide();
+    });
+}
+
+
 const scrollTopBtn = document.getElementById('scrollTopBtn');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 200) {
-        scrollTopBtn.style.display = 'block';
-    } else {
-        scrollTopBtn.style.display = 'none';
-    }
-});
-
-scrollTopBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
+if (scrollTopBtn) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 200) {
+            scrollTopBtn.style.display = 'block';
+        } else {
+            scrollTopBtn.style.display = 'none';
+        }
     });
-});
+
+    scrollTopBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
