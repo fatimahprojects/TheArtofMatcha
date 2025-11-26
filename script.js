1b08905ba6b315b1e6573e3c4b8e8fe358ab83f3
Fatimah Albariqi <fatemahh744@gmail.com>
2025-11-26 16:53:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ prevBtn.addEventListener('click', () => {
     index = (index - 1 + images.length) % images.length;
     showSlide();
 });
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollTopBtn.style.display = 'block';
+    } else {
+        scrollTopBtn.style.display = 'none';
+    }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
